dbbbf8859db13e48a54c1b6477a76a2eda9ecba5
horecio <horecio@gmail.com>
2014-03-28 10:53:09 -0300

    Local.php adicionado

---
+++ config/autoload/module.doctrine-mongo-odm.local.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            'odm_default' => array(
+                'server' => 'localhost',
+                'port' => '27017',
+            ),
+        ),      
+    ),
+);
